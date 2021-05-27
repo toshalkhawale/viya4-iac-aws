02933068582f8f0de4fa349c292033745d09caae
toshal khawale <toshal.khawale@gmail.com>
2021-05-27 18:32:32 +0530

    done with install

--- main.tf
+++ main.tf
@@ -221,7 +221,7 @@ module "jump" {
   create_vm      = var.create_jump_vm
   vm_type        = var.jump_vm_type
   vm_admin       = var.jump_vm_admin
-  ssh_public_key = file(var.ssh_public_key)
+  ssh_public_key = var.ssh_public_key
 
   cloud_init = data.template_cloudinit_config.jump.rendered
 
@@ -297,7 +297,7 @@ module "nfs" {
   create_vm      = var.storage_type == "standard" ? true : false
   vm_type        = var.nfs_vm_type
   vm_admin       = var.nfs_vm_admin
-  ssh_public_key = file(var.ssh_public_key)
+  ssh_public_key = var.ssh_public_key
 
   cloud_init = var.storage_type == "standard" ? data.template_cloudinit_config.nfs.0.rendered : null
 }
